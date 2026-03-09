a92cb3577d705c3c8dd5f294bab8513075b695f0
DeeaZaharia <zahariadeea@gmail.com>
2026-03-09 12:22:58 +0000

    Feat: Add Airport Information Class

--- src/main/java/com/still_processing/flight_data/Airport.java
+++ src/main/java/com/still_processing/flight_data/Airport.java
@@ -1,4 +1,24 @@
 package com.still_processing.flight_data;
 
 public class Airport {
+    long id;
+    String name;
+    float latitude;
+    float longitude;
+    String country;
+    String region;
+    String municipality;
+    String iataCode;
+
+    Airport(long id, String name, float latitude, float longitude, String country, String region,  String municipality, String iataCode){
+      this.id = id;
+      this.name = name;
+      this.latitude = latitude;
+      this.longitude = longitude;
+      this.country = country;
+      this.region = region;
+      this.municipality = municipality;
+      this.iataCode = iataCode;
+    }
+
 }
