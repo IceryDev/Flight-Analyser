8bdf83782deb4f1f658a6d992b10ff9df1575147
B33th03n960 <zhousun06@gmail.com>
2026-03-10 14:54:11 +0000

    Feat: added body panel alongside with TextAreaBuilder class

--- src/main/java/com/still_processing/Application/MainWindow.java
+++ src/main/java/com/still_processing/Application/MainWindow.java
@@ -1,10 +1,12 @@
 package com.still_processing.Application;
 
-import javax.swing.JFrame;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 
-import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ImageIcon;
 
 import static com.still_processing.DefaultSettings.Settings.*;
 
@@ -14,19 +16,20 @@ import static com.still_processing.DefaultSettings.Settings.*;
 public class MainWindow extends JFrame {
 
     public MainWindow() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Flight Analyser");
-        this.setSize(1024, 720);
-        this.setVisible(true);
-        this.getContentPane().setBackground(BACKGROUND);
-
         ImageIcon image = new ImageIcon(getClass().getResource("/Images/icon.jpeg"));
         this.setIconImage(image.getImage());
 
-        this.setLayout(new FlowLayout());
-        JLabel label = new JLabel();
-        label.setText("Flight Analyser");
-        label.setFont(BOLD_FONT.deriveFont(50f));
-        this.add(label);
+        JPanel body = new BodyPanel();
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportView(body);
+        scrollPane.getViewport().setBackground(BACKGROUND);
+        this.add(scrollPane, BorderLayout.CENTER);
+
+        this.setTitle("Flight Analyser");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(BACKGROUND);
+        this.setSize(1024, 720);
+        this.setMinimumSize(new Dimension(512, 512));
+        this.setVisible(true);
     }
 }
