f7be81653e87aaa374488d3fcdec2fadd5ef3237
IceryDev <71143180+IceryDev@users.noreply.github.com>
2026-03-10 15:50:20 +0000

    Feat: Create ButtonBuilder class for UILib

--- src/main/java/com/still_processing/Main.java
+++ src/main/java/com/still_processing/Main.java
@@ -9,10 +9,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("==== Flight Analyser Application ====");
 
-        // System.setProperty("sun.java2d.uiScale", "1");
-        // System.setProperty("sun.java2d.opengl", "true");
-        // System.setProperty("awt.useSystemAAFontSettings", "on");
-        // System.setProperty("swing.aatext", "true");
+         System.setProperty("sun.java2d.uiScale", "1");
+         System.setProperty("sun.java2d.opengl", "true");
+         System.setProperty("awt.useSystemAAFontSettings", "on");
+         System.setProperty("swing.aatext", "true");
 
         new MainWindow();
     }
