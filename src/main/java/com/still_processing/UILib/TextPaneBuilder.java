532174daf960f392290d86a7ca64166f00c9ba59
B33th03n960 <zhousun06@gmail.com>
2026-03-10 22:52:01 +0000

    Feat: added TextPaneBuilder (replacement of TextAreaBuilder)

---
+++ src/main/java/com/still_processing/UILib/TextPaneBuilder.java
@@ -0,0 +1,68 @@
+package com.still_processing.UILib;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JTextPane;
+import javax.swing.text.DefaultCaret;
+
+import static com.still_processing.DefaultSettings.Settings.*;
+
+public class TextPaneBuilder {
+    String text = "";
+    int fontSize = 12;
+    Font font = REGULAR_FONT;
+    Color foreground = TEXT_COLOR;
+    Color background = BACKGROUND;
+
+    public JTextPane build() {
+        JTextPane textPane = new JTextPane();
+
+        // Disable Edits
+        textPane.setEditable(false);
+        textPane.getCaret().setVisible(false);
+        textPane.setCaret(new DefaultCaret() {
+            @Override
+            public void paint(Graphics g) {
+            }
+        });
+
+        // settings
+        textPane.setText(text);
+        textPane.setForeground(foreground);
+        textPane.setBackground(background);
+        textPane.setSelectionColor(HIGHLIGHT);
+        textPane.setFont(font.deriveFont((float) fontSize));
+        textPane.setOpaque(false);
+        textPane.setSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
+
+        return textPane;
+    }
+
+    public TextPaneBuilder setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public TextPaneBuilder setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+        return this;
+    }
+
+    public TextPaneBuilder setFont(Font font) {
+        this.font = font;
+        return this;
+    }
+
+    public TextPaneBuilder setForeground(Color foreground) {
+        this.foreground = foreground;
+        return this;
+    }
+
+    public TextPaneBuilder setBackground(Color background) {
+        this.background = background;
+        return this;
+    }
+}
