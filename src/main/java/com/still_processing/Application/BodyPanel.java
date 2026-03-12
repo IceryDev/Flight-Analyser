4dfadbc665fdb7c3b2c6ef751355a409f19791b7
DeeaZaharia <zahariadeea@gmail.com>
2026-03-12 10:38:22 +0000

    Feat: Add Buttons to Body Panel

--- src/main/java/com/still_processing/Application/BodyPanel.java
+++ src/main/java/com/still_processing/Application/BodyPanel.java
@@ -4,16 +4,16 @@ import java.awt.Rectangle;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
-import javax.swing.BoxLayout;
-import javax.swing.JPanel;
-import javax.swing.Scrollable;
+import javax.swing.*;
+import java.awt.Component;
 
-import javax.swing.JTextPane;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 
 import static com.still_processing.DefaultSettings.Settings.*;
+
+import com.still_processing.UILib.ButtonBuilder;
 import com.still_processing.UILib.TextPaneBuilder;
 
 /**
@@ -37,7 +37,19 @@ class BodyPanel extends JPanel implements Scrollable {
         StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
         doc.setParagraphAttributes(0, doc.getLength(), center, false);
 
+        JPanel panel = new JPanel();
+        JButton button1 = new JButton("Search");
+        button1.setAlignmentX(Component.LEFT_ALIGNMENT);
+        button1.setBackground(GRAY);
+        button1.setSize(30, 30);
+
+        JButton button2 = new ButtonBuilder().setSize(25,25).setFontSize(15).setBackground(HIGHLIGHT).setText("From").setBorderWidth(2).build();
+        JButton button3 = new ButtonBuilder().setSize(25,25).setFontSize(15).setBackground(HIGHLIGHT).setText("To").setBorderWidth(2).build();
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button1);
         this.add(textPane);
+        this.add(panel);
     }
 
     @Override
