89427ecd405397cd60848d205d54aa4fa2363ba4
B33th03n960 <zhousun06@gmail.com>
2026-03-12 09:04:26 +0000

    Feat: added getter for access outside of package

--- src/main/java/com/still_processing/FlightData/Airport.java
+++ src/main/java/com/still_processing/FlightData/Airport.java
@@ -1,26 +1,26 @@
 package com.still_processing.FlightData;
 
 public class Airport {
-    long id;
-    String name;
-    float latitude;
-    float longitude;
-    String country;
-    String region;
-    String municipality;
-    String iataCode;
+    public long id;
+    public String name;
+    public float latitude;
+    public float longitude;
+    public String country;
+    public String region;
+    public String municipality;
+    public String iataCode;
 
     Airport(){}
 
     Airport(long id, String name, float latitude, float longitude, String country, String region,  String municipality, String iataCode){
-      this.id = id;
-      this.name = name;
-      this.latitude = latitude;
-      this.longitude = longitude;
-      this.country = country;
-      this.region = region;
-      this.municipality = municipality;
-      this.iataCode = iataCode;
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.country = country;
+        this.region = region;
+        this.municipality = municipality;
+        this.iataCode = iataCode;
     }
 
 }
--- src/main/java/com/still_processing/FlightData/Database.java
+++ src/main/java/com/still_processing/FlightData/Database.java
@@ -1,8 +1,13 @@
 package com.still_processing.FlightData;
 
-
+import java.util.Map;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Database {
     static HashMap<String, Airport> airports = new HashMap<>();
+
+    public static Map<String, Airport> getAirports() {
+        return Collections.unmodifiableMap(airports);
+    }
 }
