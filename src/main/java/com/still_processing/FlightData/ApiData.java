5c80d493a33f173885bdeb995bac4dc77261b8f7
nheatyon <markz@Marcos-MacBook-Air.local>
2026-03-12 10:43:32 +0000

    Fix: Method for extracting live flights data modified

--- src/main/java/com/still_processing/FlightData/ApiData.java
+++ src/main/java/com/still_processing/FlightData/ApiData.java
@@ -6,9 +6,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.still_processing.DefaultSettings.Settings;
 import com.still_processing.DefaultSettings.Settings.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static com.still_processing.FlightData.CheckDate.convertToString;
@@ -45,39 +47,36 @@ public class ApiData {
         }
     }
 
-    public static HttpResponse<String> liveData() { //daily flight statistics and routes
-        String currentDay = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
-
-        HttpClient client = HttpClient.newHttpClient();
+    /**
+     * Extract current live flights data for a given airport IATA code.
+     *
+     * @author Marco Fontana
+     * @param iataCode The IATA code of the airport for which to fetch live flight data
+    */
+    public static HttpResponse<String> extractLiveData(String iataCode) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'");
+        String startingCurrentDate = LocalDateTime.now().format(formatter);
+        // Todo: Data range handling needs to be fixed
+        String endingCurrentDate = startingCurrentDate + "23:59";
+        startingCurrentDate = startingCurrentDate + "00:00";
+        URI uri = URI.create("https://aerodatabox.p.rapidapi.com/flights/airports/Iata/" +  iataCode + "/" + startingCurrentDate + "/" + endingCurrentDate);
+        System.out.println(uri.getPath());
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://aerodatabox.p.rapidapi.com/airports/iata/YYZ/stats/routes/daily/" + currentDay))
+                .uri(uri)
                 .header("x-rapidapi-host", "aerodatabox.p.rapidapi.com")
                 .header("x-rapidapi-key", Settings.API_KEY)
                 .GET()
                 .build();
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            return response;
-        } catch (IOException e) {
-            System.out.println("IOException in liveData: " + e.getMessage());
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
             return null;
-        } catch (InterruptedException e) {
-            System.out.println("InterruptedException in liveData: " + e.getMessage());
-            Thread.currentThread().interrupt();
-            return null;
-        }finally {
-            client.close();
         }
     }
 
-    // For Testing:
-//    public static void main(String[] args){
-//        historicalData(5, 3, 2025, 10, 3, 2025);
-//        try{
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            Thread.currentThread().interrupt();
-//        }
-//        liveData();
-//    }
+    public static void main(String[] args) {
+        // Testing
+        System.out.println(extractLiveData("YYC").body());
+    }
 }
