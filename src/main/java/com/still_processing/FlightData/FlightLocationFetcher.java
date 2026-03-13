c92ac8eaa449f37da8ca56064af721c6895aa6f8
IceryDev <71143180+IceryDev@users.noreply.github.com>
2026-03-13 13:55:14 +0000

    Demo: Add JavaDoc

--- src/main/java/com/still_processing/FlightData/FlightLocationFetcher.java
+++ src/main/java/com/still_processing/FlightData/FlightLocationFetcher.java
@@ -3,6 +3,11 @@ package com.still_processing.FlightData;
 import java.net.http.HttpClient;
 import java.time.Instant;
 
+/**
+ * Fetches OpenSky flight data
+ *
+ * @author IceryDev
+ */
 public class FlightLocationFetcher {
 
     private final String TOKEN_ADDRESS = "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token";
