da5887c55dab02a92c2d845c8185f293b18f9ad3
Jagoda Koczwara-Szuba <jagnat.pierwszy@gmail.com>
2026-03-18 12:57:19 +0000

    Fix: Apply changes to DropdownBuilder

--- src/main/java/com/still_processing/UILib/DropdownBuilder.java
+++ src/main/java/com/still_processing/UILib/DropdownBuilder.java
@@ -1,32 +1,24 @@
 package com.still_processing.UILib;
 
-import javax.accessibility.Accessible;
-import javax.swing.JComboBox;
-
 import static com.still_processing.DefaultSettings.Settings.*;
 
+import javax.swing.JComboBox;
+
 import javax.swing.border.LineBorder;
-import javax.swing.event.ListDataListener;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import java.util.Vector;
 
 /**
  * @author Jagoda Koczwara-Szuba
  */
 
-//public class JComboBox<E> extends JComponent
-//        implements ItemSelectable, ListDataListener,ActionListener, Accessible {
-//        }
-
-//extends JComponent implements ItemSelectable, ListDataListener,ActionListener, Accessible
-
 public class DropdownBuilder {
 
     private final int[] size = new int[2];
+
+    {
+        size[1] = 50;
+    }
 
     private Color background = BACKGROUND;
     private Color foreground = TEXT_COLOR;
@@ -56,20 +48,6 @@ public class DropdownBuilder {
         if (hasBorder) {
             dropdown.setBorder(new LineBorder(Color.BLACK, borderWidth));
         }
-
-        dropdown.setMaximumRowCount(10);
-
-//        dropdown.addActionListener(e -> {
-//            String selected = (String) dropdown.getSelectedItem();
-//            System.out.println("Selected: " + selected);
-//        });
-
-//        if (listener != null) {
-//            dropdown.addActionListener(e -> {
-//                JComboBox source = (JComboBox) e.getSource();
-//                System.out.println("Selected: " + source.getSelectedItem());
-//            });
-//        }
 
         return dropdown;
     }
@@ -114,9 +92,4 @@ public class DropdownBuilder {
         this.options = options;
         return this;
     }
-
-//    public DropdownBuilder select(ActionListener listener) {
-//        this.listener = listener;
-//        return this;
-//    }
 }
