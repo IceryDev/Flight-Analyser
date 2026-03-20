08ed56b17aaaa3b76ccbca52f556dbd74d4a9764
Jagoda Koczwara-Szuba <jagnat.pierwszy@gmail.com>
2026-03-20 17:02:21 +0000

    Fix: Change double to float

--- src/main/java/com/still_processing/FlightData/Statistics.java
+++ src/main/java/com/still_processing/FlightData/Statistics.java
@@ -9,51 +9,54 @@ import java.lang.Math;
 
 public class Statistics {
 
-    public static double arithmeticMean(int[] values) {
-        return ((double) Arrays.stream(values).sum())/values.length;
+    public static float arithmeticMean(int[] values) {
+        return ((float) Arrays.stream(values).sum())/values.length;
     }
 
-    public static double arithmeticMean(double[] values) {
-        return Arrays.stream(values).sum()/values.length;
+    public static float arithmeticMean(float[] values) {
+        float sum = 0;
+        for (float value : values)
+            sum += value;
+        return sum/values.length;
     }
 
-    public static double median(int[] values) {
+    public static float median(int[] values) {
         Arrays.sort(values);
         if (values.length % 2 == 0)
-            return (double) ((values[values.length/2-1] + values[(values.length/2)])/2);
+            return (float) ((values[values.length/2-1] + values[(values.length/2)])/2);
         else
             return values[values.length/2];
     }
 
-    public static double median(double[] values) {
+    public static float median(float[] values) {
         Arrays.sort(values);
         if (values.length % 2 == 0)
-            return (double) ((values[values.length/2-1] + values[(values.length/2)])/2);
+            return (values[values.length/2-1] + values[(values.length/2)])/2;
         else
             return values[values.length/2];
     }
 
-    public static double variance(int[] values) {
-        double mean = arithmeticMean(values);
-        double sum = 0;
+    public static float variance(int[] values) {
+        float mean = arithmeticMean(values);
+        float sum = 0;
         for (int value : values)
-            sum += Math.pow(value-mean, 2);
+            sum += (float) Math.pow(value-mean, 2);
         return sum/values.length;
     }
 
-    public static double variance(double[] values) {
-        double mean = arithmeticMean(values);
-        double sum = 0;
-        for (double value : values)
-            sum += Math.pow(value-mean, 2);
+    public static float variance(float[] values) {
+        float mean = arithmeticMean(values);
+        float sum = 0;
+        for (float value : values)
+            sum += (float) Math.pow(value-mean, 2);
         return sum/values.length;
     }
 
-    public static double standardDeviation(int[] values) {
-        return Math.sqrt(variance(values));
+    public static float standardDeviation(int[] values) {
+        return (float) Math.sqrt(variance(values));
     }
 
-    public static double standardDeviation(double[] values) {
-        return Math.sqrt(variance(values));
+    public static float standardDeviation(float[] values) {
+        return (float) Math.sqrt(variance(values));
     }
 }
