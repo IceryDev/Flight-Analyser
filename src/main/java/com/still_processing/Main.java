bf5c8592d71896bd06d948c9fd01cbfbefe9850e
B33th03n960 <zhousun06@gmail.com>
2026-03-21 22:44:55 +0000

    chore: clean up bad settings

--- src/main/java/com/still_processing/Main.java
+++ src/main/java/com/still_processing/Main.java
@@ -2,11 +2,12 @@ package com.still_processing;
 
 import javax.swing.UIManager;
 import javax.swing.BorderFactory;
+import javax.swing.SwingUtilities;
 
-import static com.still_processing.DefaultSettings.Settings.*;
 import com.still_processing.FlightData.CSVHandler;
 import com.still_processing.Application.MainWindow;
 import com.still_processing.FlightData.FlightFetcher;
+import static com.still_processing.DefaultSettings.Settings.*;
 
 /**
  * Application Entry point
@@ -18,19 +19,18 @@ public class Main {
         CSVHandler.loadOfflineFlightCSV();
         FlightFetcher.getAirlineCodes();
 
-        System.setProperty("sun.java2d.uiScale", "1");
+        System.setProperty("sun.java2d.uiScale", "2.0");
         System.setProperty("sun.java2d.opengl", "true");
         System.setProperty("awt.useSystemAAFontSettings", "on");
         System.setProperty("swing.aatext", "true");
 
         try {
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             UIManager.getDefaults().put("TableHeader.cellBorder", BorderFactory.createMatteBorder(5, 0, 5, 0, GRAY));
-            UIManager.getDefaults().put("TableBody.cellBorder", BorderFactory.createMatteBorder(5, 0, 5, 0, GRAY));
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        new MainWindow().setVisible(true);
+        SwingUtilities.invokeLater(() -> new MainWindow().setVisible(true));
     }
 }
