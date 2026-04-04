b278382fccdcfbfadcd07bf744a1f4781822b3a8
jesschen8 <jessicachen1886@gmail.com>
2026-04-04 23:54:24 +0100

    Feat: Add hover effect to RoundedButton.java.

--- src/main/java/com/still_processing/UILib/RoundedButton.java
+++ src/main/java/com/still_processing/UILib/RoundedButton.java
@@ -6,28 +6,51 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.RoundRectangle2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import static com.still_processing.DefaultSettings.Settings.*;
 
 public class RoundedButton extends JButton {
     private Color backgroundColor;
     private int radius;
+    private Color hoverColor;
+    private boolean isHovered = false;
 
-    public RoundedButton(String text, int radius, Color backgroundColor){
+    public RoundedButton(String text, int radius, Color backgroundColor, Color hoverColor){
         super(text);
         this.radius = radius;
         this.backgroundColor = backgroundColor;
+        this.hoverColor = hoverColor;
 
         setContentAreaFilled(false);
         setBorderPainted(false);
         setOpaque(false);
         setFocusPainted(false);
+
+        addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                isHovered = true;
+                repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                isHovered = false;
+                repaint();
+            }
+        });
     }
     @Override
     protected void paintComponent(Graphics g){
         Graphics2D g2d = (Graphics2D) g.create();
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setColor(backgroundColor);
+        if(isHovered){
+            g2d.setColor(hoverColor);
+        }
+        else{
+            g2d.setColor(backgroundColor);
+        }
         g2d.fill(new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), radius, radius));
         g2d.dispose();
         super.paintComponent(g);
@@ -37,4 +60,6 @@ public class RoundedButton extends JButton {
     public boolean contains(int x, int y) {
         return new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), radius, radius).contains(x, y);
     }
+
+
 }
